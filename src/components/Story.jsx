29f1a2753d1290f9c13da1abf6b6c4ad85adeb17
anaselass <anas.elassri.01@gmail.com>
2025-07-02 18:37:49 +0100

    Add interactive mouse effects and RoundedCorners component to Story

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,6 +1,50 @@
 import AnimatedTitle from "./AnimatedTitle";
+import RoundedCorners from "./RoundedCorners";
+import { useRef } from "react";
+import gsap from "gsap";
+import Button from "./Button";
 
 const Story = () => {
+  const frameRef = useRef("null");
+
+  const handleMouseLeave = () => {
+    const element = frameRef.current;
+
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX: 0,
+      rotateY: 0,
+
+      ease: "power1.inOut",
+    });
+  };
+
+  const handleMouseMove = (e) => {
+    const { clientX, clientY } = e;
+    const element = frameRef.current;
+
+    if (!element) return;
+
+    const rect = element.getBoundingClientRect();
+
+    const x = clientX - rect.left;
+    const y = clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = ((y - centerY) / centerY) * -10;
+    const rotateY = ((x - centerX) / centerX) * 10;
+
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX,
+      rotateY,
+      transformPerspective: 500,
+      ease: "power1.inOut",
+    });
+  };
+
   return (
     <section id="story" className="min-h-dvh w-screen bg-black text-blue-50">
       <div className="flex size-full flex-col items-center py-10 pb-24">
@@ -21,12 +65,36 @@ const Story = () => {
             <div className="story-img-mask">
               <div className="story-img-content">
                 <img
+                  ref={frameRef}
+                  onMouseLeave={handleMouseLeave}
+                  onMouseUp={handleMouseLeave}
+                  onMouseEnter={handleMouseLeave}
+                  onMouseMove={handleMouseMove}
                   src="/img/entrance.webp"
                   alt="entrance"
                   className="object-contain"
                 />
               </div>
             </div>
+
+            <RoundedCorners />
+          </div>
+        </div>
+
+        <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
+          <div className="flex h-full w-fit flex-col items-center md:items-start">
+            <p className="mt-6 max-w-sm font-circular-web text-violet-50 text-center md:text-start">
+              {" "}
+              Where realms converge, lies Zentry and the boundless pillar.
+              Discover its secrets and shape your fate amidst infinite
+              opportunities.
+            </p>
+
+            <Button
+              id="realm-btn"
+              title="discover prologue"
+              containerClass="mt-5"
+            />
           </div>
         </div>
       </div>
